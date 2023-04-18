f5f018c0d6763b727448bfb8bfbbf205e45c2096
atharva-4 <112089821+atharva-4@users.noreply.github.com>
2023-04-18 21:45:52 +0530

    Update script.js

--- css3/script.js
+++ css3/script.js
@@ -1,23 +1,41 @@
 // Television JS //
 
 function play() { 
-var video = document.getElementById("vid"); 
-var button = document.getElementById("btn"); 
-var scr = document.getElementById("tv-scr");
+    var video = document.getElementById("vid"); 
+    var button = document.getElementById("btn"); 
+    var scr = document.getElementById("tv-scr");
+    
+    if (video.paused) { 
+    video.play();
+    video.style.width = 'auto';
+    button.style.backgroundColor = 'green';
+    button.style.animation = 'none';
+    scr.style.boxShadow = '1px 2px 5px white';
+     } 
+    else 
+    { 
+    video.pause();
+    video.style.width = '0px';
+    button.style.backgroundColor = '#b80d0d';
+    button.style.animation = '';
+    scr.style.boxShadow = 'none';
+    } 
+    }
 
-if (video.paused) { 
-video.play();
-video.style.width = 'auto';
-button.style.backgroundColor = 'green';
-button.style.animation = 'none';
-scr.style.boxShadow = '1px 2px 5px white';
- } 
-else 
-{ 
-video.pause();
-video.style.width = '0px';
-button.style.backgroundColor = '#b80d0d';
-button.style.animation = '';
-scr.style.boxShadow = 'none';
-} 
-} 
+    function playsong(){
+        var audio = document.getElementById("aud");
+        var speaker = document.getElementById("speake");
+        var radio = document.getElementById("radi");
+        var button2 = document.getElementById("btn2");
+        var song_name = document.getElementById("name");
+
+        if (audio.paused) { 
+            audio.play();
+            button2.style.filter = 'filter: drop-shadow(0 0 20px #ffff99)';
+             } 
+        else 
+            { 
+            audio.pause();
+            button2.style.filter = 'none';
+            } 
+    }
