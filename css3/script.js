1bf04718806a4dec59e184d43e531b594349d988
atharva-4 <112089821+atharva-4@users.noreply.github.com>
2023-04-18 15:48:06 +0530

    Update script.js

--- css3/script.js
+++ css3/script.js
@@ -1,1 +1,21 @@
-.
+function play() { 
+var video = document.getElementById("vid"); 
+var button = document.getElementById("btn"); 
+var scr = document.getElementById("tv-scr");
+
+if (video.paused) { 
+video.play();
+video.style.width = 'auto';
+button.style.backgroundColor = 'green';
+button.style.animation = 'none';
+scr.style.boxShadow = '1px 2px 5px white';
+ } 
+else 
+{ 
+video.pause();
+video.style.width = '0px';
+button.style.backgroundColor = '#b80d0d';
+button.style.animation = '';
+scr.style.boxShadow = 'none';
+} 
+} 
