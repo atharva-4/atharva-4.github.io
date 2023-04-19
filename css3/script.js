6c628f2b25237c139a87f256778c683390616408
atharva-4 <112089821+atharva-4@users.noreply.github.com>
2023-04-19 08:48:12 +0530

    update script.js

--- css3/script.js
+++ css3/script.js
@@ -23,19 +23,36 @@ function play() {
     }
 
     function playsong(){
-        var audio = document.getElementById("aud");
-        var speaker = document.getElementById("speake");
-        var radio = document.getElementById("radi");
-        var button2 = document.getElementById("btn2");
+        var wall = document.getElementById("wall");
+        var speaker = document.getElementById("speaker");
+        var audio = document.getElementById("audio");
+        var button2 = document.getElementById("radio-btn");
         var song_name = document.getElementById("name");
-
+        
+        var css_of_button2_bf = window.getComputedStyle(button2, '::before');
+        
+        var css_of_speaker_bf = window.getComputedStyle(button2, '::before', '::after');
+        
         if (audio.paused) { 
             audio.play();
-            button2.style.filter = 'filter: drop-shadow(0 0 20px #ffff99)';
-             } 
-        else 
-            { 
+            button2.style.setProperty('--background', '#ffff99');
+            button2.style.setProperty('--filter1', 'drop-shadow(0 0 20px #ffff99)');
+            button2.style.setProperty('--animation1', 'radio-fade 3s linear infinite');
+            song_name.style.setProperty('background', '#0c8d07');
+            song_name.style.setProperty('filter','drop-shadow(0 0 8px #0c8d07)');
+            song_name.style.setProperty('--content2', 'visible');
+            speaker.style.setProperty('--animation2', 'speaker-fade 2s linear infinite');
+            
+        }
+        else {
             audio.pause();
             button2.style.filter = 'none';
+            button2.style.setProperty('--background', '#b4b447');
+            button2.style.setProperty('--filter1', '');
+            button2.style.setProperty('--animation1', '');
+            song_name.style.setProperty('background', '#494747');
+            song_name.style.setProperty('filter', '');
+            song_name.style.setProperty('--content2', '');
+            speaker.style.setProperty('--animation2', '');
             } 
     }
